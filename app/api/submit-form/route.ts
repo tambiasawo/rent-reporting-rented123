34f2ffbfda325defc69dca81fc9dbd1619c886c0
Tambi <tambi.asawo@verticalmotion.ca>
2025-03-31 12:59:25 -0700

    feat: add submit api

--- app/api/submit-form/route.ts
+++ app/api/submit-form/route.ts
@@ -1,8 +1,32 @@
-import { NextResponse } from 'next/server';
+import { NextResponse } from "next/server";
 
 export async function POST(request: Request) {
   const data = await request.json();
-  console.log('Form submission:', data);
-  
-  return NextResponse.json({ message: 'Form submitted successfully' });
+
+  try {
+    const response = await fetch(
+      "https://7c8ctvqarg.execute-api.us-west-2.amazonaws.com/submit-tenant-data",
+      {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify(data),
+      }
+    );
+    const responseData = await response.json();
+    console.log({ responseData, response });
+    if (response.ok)
+      return NextResponse.json({
+        success: response.ok,
+        message: responseData.message,
+      });
+    else {
+      return NextResponse.json({
+        success: false,
+        message: "An error occured",
+      });
+    }
+  } catch (err) {
+    console.error("Lambda error:", err);
+    //res.status(500).json({ error: "Failed to call Lambda" });
+  }
 }
